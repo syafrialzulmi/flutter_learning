a4c415419019b930888e882697bb9000972a4b06
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-04-19 08:34:42 +0700

    add elogin5

--- lib/module/online_class/flutter_book/elogin5/view/elogin5_view.dart
+++ lib/module/online_class/flutter_book/elogin5/view/elogin5_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/core.dart';
-import '../controller/elogin5_controller.dart';
 
 class Elogin5View extends StatefulWidget {
   const Elogin5View({Key? key}) : super(key: key);
@@ -9,15 +8,147 @@ class Elogin5View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Elogin5"),
-        actions: const [],
-      ),
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(30),
+          height: MediaQuery.of(Get.currentContext).size.height,
           child: Column(
-            children: const [],
+            children: [
+              const Expanded(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Icon(
+                    Icons.save_outlined,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                  child: Image.asset(
+                    'assets/icons/magicbook_logo.png',
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff59B58D),
+                        elevation: 0,
+                        fixedSize:
+                            Size(MediaQuery.of(context).size.width * 1, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Row(
+                        children: const [
+                          Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.square,
+                              color: Color(0xff3F0606),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              "Log In with Google",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff59B58D),
+                        elevation: 0,
+                        fixedSize:
+                            Size(MediaQuery.of(context).size.width * 1, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Row(
+                        children: const [
+                          Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.square,
+                              color: Color(0xff3F0606),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              "Log In with Facebook",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff59B58D),
+                        elevation: 0,
+                        fixedSize:
+                            Size(MediaQuery.of(context).size.width * 1, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Row(
+                        children: const [
+                          Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.square,
+                              color: Color(0xff3F0606),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              "Log In as Guest",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
