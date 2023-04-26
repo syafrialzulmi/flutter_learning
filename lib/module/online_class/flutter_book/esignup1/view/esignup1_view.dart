1d3ac55c2db76d973e331892d35e0f4823149ffc
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-04-26 13:48:15 +0700

    add esignup 1 and 2

--- lib/module/online_class/flutter_book/esignup1/view/esignup1_view.dart
+++ lib/module/online_class/flutter_book/esignup1/view/esignup1_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/core.dart';
 import '../controller/esignup1_controller.dart';
+import 'package:flutter/gestures.dart';
 
 class Esignup1View extends StatefulWidget {
   const Esignup1View({Key? key}) : super(key: key);
@@ -9,15 +10,240 @@ class Esignup1View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Esignup1"),
-        actions: const [],
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          'assets/icons/magicbook_logo.png',
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        leading: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Color(0xff59B58D),
+        ),
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 50,
+          ),
           child: Column(
-            children: const [],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffF4F6F6),
+                  elevation: 0,
+                  fixedSize: Size(MediaQuery.of(context).size.width * 1, 48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Log In with Facebook",
+                      style: TextStyle(
+                        color: Color(0xff455154),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Icon(
+                      Icons.facebook,
+                      color: Color(0xffC4CACC),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "or sign up with Email",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff969FA2),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'First Name',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Last Name',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Email',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.remove_red_eye,
+                    color: Color(0xffC4CACC),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Password',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.remove_red_eye,
+                    color: Color(0xffC4CACC),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Password',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff59B58D),
+                  elevation: 0,
+                  fixedSize: Size(MediaQuery.of(context).size.width * 1, 48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: "By signing up, you agree to our",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff969FA2),
+                      ),
+                    ),
+                    TextSpan(
+                      text: "\nTerms",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff59B58D),
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("nTerms");
+                        },
+                    ),
+                    const TextSpan(
+                      text: " and ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff969FA2),
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Privacy",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff59B58D),
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("Privacy");
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/module/online_class/flutter_book/esignup2/view/esignup2_view.dart
+++ lib/module/online_class/flutter_book/esignup2/view/esignup2_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/core.dart';
 import '../controller/esignup2_controller.dart';
@@ -9,15 +10,217 @@ class Esignup2View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Esignup2"),
-        actions: const [],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff455154),
+          ),
+        ),
+        leading: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Color(0xff59B58D),
+        ),
+        actions: const [
+          Icon(
+            Icons.close,
+            color: Color(0xff59B58D),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 30,
+          ),
           child: Column(
-            children: const [],
+            children: [
+              SizedBox(
+                height: 120,
+                width: 120,
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  fit: StackFit.expand,
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Color(0xffF4F6F6),
+                      radius: 60,
+                      child: Icon(
+                        Icons.person,
+                        size: 60,
+                        color: Color(0xffC4CACC),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: -25,
+                      child: RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: const Color(0xff59B58D),
+                        shape: const CircleBorder(),
+                        child: const Icon(
+                          Icons.camera_alt,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Full Name',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Email',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.remove_red_eye,
+                    color: Color(0xffC4CACC),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF4F6F6),
+                  labelText: 'Password',
+                  labelStyle: const TextStyle(
+                    color: Color(0xff455154),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff59B58D),
+                  elevation: 0,
+                  fixedSize: Size(MediaQuery.of(context).size.width * 1, 48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text(
+                  "Create an Account",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: "By signing up, you agree to our",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff969FA2),
+                      ),
+                    ),
+                    TextSpan(
+                      text: "\nTerms",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff59B58D),
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("nTerms");
+                        },
+                    ),
+                    const TextSpan(
+                      text: " and ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff969FA2),
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Privacy",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff59B58D),
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("Privacy");
+                        },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: const Color(0xff59B58D),
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text("I have an account"),
+              ),
+            ],
           ),
         ),
       ),
