16b1f04a849678421c2bbfccc6046cbee95eaf53
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-04-27 05:46:42 +0700

    dashboard 3

--- lib/module/online_class/flutter_book/edashboard3/view/edashboard3_view.dart
+++ lib/module/online_class/flutter_book/edashboard3/view/edashboard3_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/core.dart';
-import '../controller/edashboard3_controller.dart';
 
 class Edashboard3View extends StatefulWidget {
   const Edashboard3View({Key? key}) : super(key: key);
@@ -9,17 +8,146 @@ class Edashboard3View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Edashboard3"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Image.asset("assets/icons/logo.png"),
+        title: const Text(
+          "Scheme Store",
+          style: TextStyle(
+            color: Color(0xff455154),
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
           ),
         ),
+        actions: const [
+          Icon(
+            Icons.account_circle_rounded,
+            color: Color(0xffAAAAAA),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: MediaQuery.of(Get.currentContext).size.width,
+              margin: const EdgeInsets.only(
+                left: 15,
+                right: 15,
+                bottom: 15,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffCDCDCD),
+              ),
+              child: contentGalery(
+                  tag: "adidas",
+                  title: "Gazelle Suede",
+                  description:
+                      "The Adidas Originals draw inspiration from street culture and retro styles."),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 15,
+                      right: 7.5,
+                      bottom: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xffCDCDCD),
+                    ),
+                    child: contentGalery(
+                        tag: "puma",
+                        title: "Soccer Boots",
+                        description:
+                            "A focus on functionality as well as style is paramount in PUMA's designs"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 7.5,
+                      right: 15,
+                      bottom: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xffCDCDCD),
+                    ),
+                    child: contentGalery(
+                        tag: "reebook",
+                        title: "Combat Boxing",
+                        description:
+                            "Reebok have aligned themselves with some of the world's top athletes"),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  contentGalery(
+      {required String tag,
+      required String title,
+      required String description}) {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                tag.toUpperCase(),
+                style: const TextStyle(
+                  color: Color(0xff455154),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Color(0xff455154),
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+          const Center(
+            child: Icon(
+              Icons.image,
+              color: Color(0xff455154),
+              size: 75,
+            ),
+          ),
+          Text(
+            description,
+            style: const TextStyle(
+              color: Color(0xff455154),
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
       ),
     );
   }
